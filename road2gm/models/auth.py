333b096e2912da94993cbe3239026cdd4592b0e9
jonghwa <jonghwa@pincoin.co.kr>
2024-11-02 23:29:34 +0900

    role entity added

--- road2gm/models/auth.py
+++ road2gm/models/auth.py
@@ -36,6 +36,28 @@ class User(model_utils_models.TimeStampedModel):
         return self.username
 
 
+class Role(model_utils_models.TimeStampedModel):
+    name = models.CharField(
+        verbose_name='아이디',
+        max_length=150,
+    )
+
+    parent = models.ForeignKey(
+        'road2gm.Role',
+        verbose_name='부모',
+        db_index=True,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = '역할'
+        verbose_name_plural = '역할'
+        db_table = 'role'
+
+    def __str__(self):
+        return self.name
+
+
 class RefreshToken(model_utils_models.TimeStampedModel):
     username = models.CharField(
         verbose_name='아이디',
